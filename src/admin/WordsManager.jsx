485e730acf6ee2be7221b23d93b81a6ad2f322c9
Jakub Olejnik <87308273+JakubOlejnik2007@users.noreply.github.com>
2025-12-07 14:18:53 +0100

    Added management to words

--- src/admin/WordsManager.jsx
+++ src/admin/WordsManager.jsx
@@ -1,12 +1,50 @@
 import { useState, useEffect } from 'react';
 import './admin.css';
-
+import CollectionCard from '../components/WordManager/CollectionCard';
+import { fetchCollections, fetchWords } from '../service/fetchFunctions';
+import { useQuery } from "@tanstack/react-query";
+import WordsEditor from '../components/WordManager/WordsEditor';
 export default function WordsManager() {
+
+  const [selectedCollection, setSelectedCollection] = useState(null)
+
+
+  const { data: collections } = useQuery({
+    queryKey: ["collections"],
+    queryFn: fetchCollections,
+    staleTime: 1000 * 60 * 5
+  });
+
+  const { data: words = [], refetch: words_refetch } = useQuery({
+    queryKey: ["words", selectedCollection],
+    queryFn: () => fetchWords(selectedCollection),
+    enabled: !!selectedCollection,
+    staleTime: 1000 * 60 * 5
+  });
+
 
 
   return (
     <div className="admin-panel">
-      <h2>📝 Zarządzanie słówkami</h2>
+      <h2>📝 Zarządzanie fiszkami</h2>
+
+      <h3>Kolekcje</h3>
+      <div style={{
+        display: "flex",
+        gap: 10,
+        margin: "1rem"
+      }} >
+        {
+          collections?.map(item => <CollectionCard {...item} onClick={() => { setSelectedCollection(item.id) }} />)
+        }
+
+      </div>
+
+      <h3 style={{ color: "black" }}>Wybrana kolekcja: {selectedCollection ? collections.find(collection => collection.id === selectedCollection).name : "Brak"}</h3>
+
+      {
+        selectedCollection && words ? <WordsEditor words={words} /> : ""
+      }
 
       <div className="words-actions">
         <input
