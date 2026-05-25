9ef6c3742ad6e3143e4609ac446c5ca6c4789d63
Jakub Olejnik <87308273+JakubOlejnik2007@users.noreply.github.com>
2026-05-25 23:04:35 +0200

    Added py script

---
+++ src/utils/python/word-splitter.py
@@ -0,0 +1,597 @@
+x ="""6 
+
+head teacher's office - gabinet dyrektora / dyrektorki 
+
+playing field - boisko sportowe 
+
+changing room - szatnia 
+
+common room - świetlica 
+
+language laboratory - pracownia językowa 
+
+Science lab - sala do nauki biologii, chemii, fizyki 
+
+7 
+
+Art - plastyka 
+
+Civic studies / Civics / Citizenship - wiedza o społeczeństwie 
+
+Computer Science - informatyka 
+
+foreign languages - języki obce 
+
+humanities - przedmioty humanistyczne 
+
+Information and Communication Technology (ICT) - informatyka 
+
+Physical Education (PE) - wychowanie fizyczne, WF 
+
+Religious Education (RE) - religia 
+
+Science (subjects) - nauki ścłste / przyrodnicze 
+
+8 
+
+assignment - zadanie (domowe), projekt 
+
+submit an assignment - oddać zadanie (domowe) 
+
+bar chart - wykres słupkowy 
+
+be off school - być nieobecnym w szkole 
+
+brainstorm - przeprowadzać burzę mózgów, dyskutować  
+
+catch up on (schoolwork) - nadrobić (materiał z zajęć) 
+
+catch up with (the rest of the class) - dogonić (resztę klasy)  
+
+child prodigy - cudowne dziecko  
+
+conduct experiments - przeprowadzać eksperymenty 
+
+copy (answers) from sb - przepisywać (odpowiedzi) od kogoś 
+
+cram - wkuwać 
+
+distinguished - wybitny, znakomity 
+
+essay - wypracowanie 
+
+excel at sth - wyróżniać się w czymś 
+
+fall behind with schoolwork - mieć zaległości w nauce  
+
+get down to work - zabrać się do pracy 
+
+gifted - uzdolniony 
+
+give feedback on / go over - omawiać 
+
+graph - wykres 
+
+mathematical formula - wzór matematyczny 
+
+note down - zapisać, zanotować pay attention (to) - uważać, słuchać uważnie 
+
+pie chart - wykres kołowy 
+
+project work - praca projektowa 
+
+read Greek myths - czytać mity greckie 
+
+required reading - lektura obowiązkowa 
+
+revision - powtórka 
+
+do some ~ - powtarzać show up late - spóżnić się 
+
+solve quadratic equations - rozwiązywać równania kwadratowe 
+
+study past papers - studiować arkusze egzaminacyjne z ubiegłych lat 
+
+take in (information) - przyswajać (informacje) 
+
+take notes - robić notatki 
+
+teamwork - praca zespołowa 
+
+test tube - probówka 
+
+worksheet - arkusz, karta ćwiczeń 
+
+write up an experiment - opisać doświadczenie 
+
+9 
+
+acquire new knowledge - zdobywać nowa wiedzę 
+
+develop your potential - rozwijać swój potencjał 
+
+distance learning - kształcenie 
+
+na odlegosć effort - wysiłek 
+
+employability - szanse na zatrudnienie 
+
+postgraduate course/studies - studia podyplomowe 
+
+pursue your goals / dreams - dążyć do realizacji celów / marzeń 
+
+set (clear) targets - wyznaczać (jasne) cele 
+
+sign up for sth - zapisać się na coś 
+
+10 
+
+a piece of cake - łatwizna, pestka  
+
+burn the midnight oil - pracować, uczyć się do późna w nocy  
+
+draw a blank - mieć pustkę w głowie 
+
+feel out of your depth - nie móc czegoś zrozumieć, nie mieć wystarczającej wiedzy 
+
+have a lot of ground to cover - mieć sporo do zrobienia, omówienia 
+
+know sth inside out - znać coś na wylot 
+
+pass (an exam) with flying colours - zdać (egzamin) śpiewająco  
+
+you can't teach an old dog new tricks - nie przesadza się starych drzew (pedofil to tłumaczył) 
+
+11 
+
+compass/compasses - cyrkiel 
+
+coursebook - podręcznik 
+
+file / ring binder - segregator 
+
+highlighter - zakreślacz 
+
+hole punch - dziurkacz 
+
+set square - ekierka 
+
+textbook - podręcznik 
+
+electronic textbook - podrecznik w wersji elektronicznej 
+
+paper textbook - podręcznik w wersji papierowej 
+
+whiteboard marker - pisak do tablic 
+
+12 
+
+academic achievements/ performance - wyniki w nauce 
+
+academic standards - poziom nauczania 
+
+achievement - osiągnięcie 
+
+assess - oceniać 
+
+assessment - ocena, ocenianie  
+
+average (marks) - przeciętne (oceny) 
+
+above average marks - oceny powyżej średniej 
+
+entrance exam - egzamin wstępny 
+
+exam results - wyniki egzaminu 
+
+examination rules - zasady zdawania egzaminu 
+
+external exam - egzamin zewnętrzny 
+
+evaluate - oceniać 
+
+evaluation - ocena, ocenianie  
+
+fail an exam / a test - nie zdać egzaminu/testu 
+
+flunk an exam - oblać egzamin  
+
+get a score - otrzymać wynik, rezultat 
+
+grade - stopień, ocena  
+
+get an A+ / A grade - otrzymać szóstkę / piątkę 
+
+grade point average (GPA) - średnia ocen 
+
+have a (high) GPA - mieć (wysoka) średnią ocen 
+
+mark (czasownik)- sprawdzać, oceniać  
+
+mark - ocena, stopień wyrażony w punktach 
+
+mock exam - egzamin próbny 
+
+multiple-choice test - test wielokrotnego wyboru 
+
+pass an exam / a test - zdać test 
+
+receive a fail - otrzymać negatywną ocenę 
+
+resit / retake an exam - zdawać egzamin ponownie 
+
+score 100 points - otrzymać sto punktów 
+
+set an exam - przygotować egzamin 
+
+sit / take an exam - przystępować do egzaminu 
+
+top grades - najlepsze oceny 
+
+13 
+
+abilities / aptitude - zdolności 
+
+announce sth in assembly - ogłosić coś na apelu 
+
+attend school - chodzić do szkoły 
+
+attendance - frekwencja, obecność 
+
+take attendance - sprawdzać listę obecności 
+
+bookworm - mól książkowy 
+
+copycat - osoba naśladująca innych 
+
+dismiss - zwolnić uczniów do domu 
+
+dread sth - obawiać się czegoś 
+
+eager beaver - osoba nadgorliwa 
+
+(electronic / online) register - dziennik (elektroniczny) 
+
+end up doing sth - tu: ostatecznie coś zrobić (wbrew wcześniejszym uczuciom / planom) 
+fieldwork - prace badawcze w terenie 
+fire drill - ćwiczenia przeciwpożarowe 
+graduation ball - bal absolwentów 
+
+grant - stypendium 
+
+hang around - kręcić się, przebywać w jakimś miejscu  
+
+homeschooling - nauczanie domowe 
+
+know-all - mądrala 
+
+look back on sth - wspominać coś 
+
+prom (AmE) - bal na zakończenie roku szkolnego w szkole średniej 
+
+restrict access to (a room) - ograniczyć dostęp do (pomieszczenia) 
+
+scholarship - stypendium 
+
+school fundraiser - szkolna zbiorka charytatywna 
+
+school report - karta ocen ucznia / świadectwo szkolne 
+
+school run - wożenie dziecka 00 i ze szkoły 
+
+set homework - zadawać prace domową 
+
+source texts - teksty źródłowe 
+
+staft meeting- rada pedagogiczna 
+
+take the register - sprawdzać listę obecności w dzienniku  
+
+teacher's pet - ulubieniec nauczyciela 
+
+time off - czas wolny 
+
+timetable - plan lekcji 
+
+yearbook - kronika szkolna 
+
+14 
+
+anti-social behaviour - zachowanie antyspołeczne 
+
+bully - osoba prześladująca / zastraszająca innych uczniow 
+
+bullying - prześladowanie 
+
+CCTV camera - kamera monitoringu 
+
+cause disruption - zakłócać porządek 
+
+cheat - oszukiwać, ściągać  
+
+crib / crib sheet - ściąga 
+
+do well/badly at school / in exams - mieć dobre/zte wyniki w szkole / na egzaminach 
+
+drop out of (school/college) - rzucić (szkołę/studia) 
+
+dropout - osoba, która nie ukończyła szkoły/studiów 
+
+dysgraphia - dysgrafia  
+
+dyslexia - dysleksja  
+
+dyslexic - dyslektyczny  
+
+educational problems - problemy edukacyne 
+
+expel sb - wydalić kogoś (ze szkoły) 
+
+expulsion - wydalenie 
+
+fail to achieve sth - nie osiagnać czegos 
+
+failure - porażka, niepowodzenie  
+
+have a hard time - mieć problemy, trudnosci 
+
+hopeless at (Maths) - beznadziejny z (matematyki) 
+
+individual approach - indywidualne podejscie 
+
+learning difficulties - trudnosci w czeniu sie 
+
+misbehave - źle się zachowywac  
+
+offence - przewinienie  
+
+overcome problems - pokonywać problemy 
+
+play truant - wagarowac  
+
+remedial classes - zajęcia wyrownawcze 
+
+skip lessons/classes - opuszczać lekcje 
+
+support - wsparcie 
+
+suspend sb from school - zawiesić kogoś w prawach ucznia 
+
+throw sb out (of school) - wydalic kogoś (ze szkoły) 
+
+15 
+
+afterschool club - swietlica szkolna / kółko zainteresowań 
+
+attend a course - chodzic/ uczęszczać na kurs 
+
+be/become a member - być / zostać członkiem 
+
+book club - klub czytelniczy 
+
+Chemistry olympiad - olimpiada chemiczna 
+
+develop (leadership/interpersonal) skills - rozwijać umiejętności (przywódcze/interpersonalne) 
+
+drama club - kółko teatralne  
+
+first aid training - kurs pierwszej pomocy 
+
+join a club - zapisać się do kółka zainteresowań 
+
+literature club - kółko literackie  
+
+participate in - brać udziat w  
+
+school choir - chór szkolny  
+
+school orchestra - orkiestra szkolna 
+
+society - towarzystwo, stowarzyszenie 
+
+voluntary participation - dobrowolne uczestnictwo 
+
+16 
+
+A-levels - brytyjski odpowiednik matury 
+
+boarding school - szkoła z internatem 
+
+certificate (of completion) - certyfikat, świadectwo (ukończenia) 
+
+college - w Wielkiej Brytanii: szkoła pomaturalna, kolegium; w USA: uniwersytet 
+
+comprehensive school - szkoła państwowa dla uczniów powyżej 11. roku życia 
+
+compulsory (subject) - (przedmiot) obowiązkowy 
+
+curriculum - program nauczania  
+
+follow the ~ - realizowac program nauczania 
+
+deputy head - wicedyrektor, winedyrektorka 
+
+enrol on a course - zapisac sie na kurs 
+
+form teacher - wychowawca, wychowawczyni 
+
+foundation year - zerówka 
+
+GCSEs - brytyjski odpowiednik byłego egzaminu gimnazjalnego 
+
+high school (AmE) - szkoła średnia 
+
+independent school - szkota niepubliczna 
+
+junior high school (AmE) - gimnazjum 
+
+mandatory education - szkolnictwo obowiązkowe 
+
+middle school (AmE) - gimnazjum  
+
+mixed-ability class - klasa złozona z uczniów o różnym poziomie umiejetności 
+
+nursery -żłobek 
+
+nursery school / kindergarten - predszkole 
+
+obligatory - obowiązkowy  
+
+preschool - przedszkole  
+
+primary school - szkoła podstawowa 
+
+private school - szkoła prywatna  
+
+public school - w Wielkiej Brytanii: szkoła prywatna; w USA: szkoła publiczna, szkoła panstwowa 
+
+schoolchild - uczen, uczennica  
+
+school-leaving exam - egzamin po ukończeniu nauki w szkole 
+
+school-leaving qualifications - kwalifikacje absolwenta szkoły 
+
+secondary school (BrE) - szkoła srednia 
+
+state education - szkolnictwo publiczne 
+
+state school (BrE) - szkoła publiczna, szkoła panstwowa 
+
+syllabus - program studiów / nauczania danego przedmiotu 
+
+exam syllabus - wymagania egzaminacyne 
+
+teaching staff - grono pedagogiczne 
+
+tertiary education - wykształcenie / szkolnictwo wyższe 
+
+vocational school- szkota zawodowa 
+
+17 
+
+academic year - rok akademicki  
+
+access course - kurs przygotowujący na studia 
+
+admission requirements - warunki przyjęcia 
+
+apply to university - ubiegać się o przyjęcie na uniersytet 
+
+attend lectures - uczęszczać na wykłady 
+
+BA (Bachelor of Arts) - licencjat (nauki humanistyczne) 
+
+bachelor's thesis - praca licencjacka 
+
+BEd (Bachelor of Education) - licencjat w dziedzinie pedagogiki 
+
+BSc (Bachelor of Science) - licencjat (nauki ściste) 
+
+campus - kampus uniwersytecki  
+
+dean - dziekan  
+
+department - wydział / katedra 
+
+do/get a degree - uzyskać stopień naukowy 
+
+doctoral thesis - praca doktorska  
+
+doctorate - doktorat  
+
+dormitory (AmE) - akademik, internat, bursa  
+
+field - dziedzina  
+
+financial assistance - wsparcie finansowe 
+
+get a scholarship/grant - dostać stypendium 
+
+get into university - dostać się na uniwersytet 
+
+graduate (from) - ukończyć  
+
+studia graduate - absolwent/ absolwentka 
+
+hall of residence / halls - akademik, internat, bursa  
+
+lecturer - wykładowca, wykładowczyni 
+
+MA (Master of Arts) - magister (nauki humanistyczne) 
+
+master's thesis - praca magisterska 
+
+MEd (Master of Education) - magister w dziedzinie pedagogiki 
+
+MSc (Master of Science) - magister (nauki ścisłe) 
+
+PhD (Doctor of Philosophy) - doktor (nauki humanistyczne i scisłe) 
+
+reject candidates - odrzucać kandydatów 
+
+research paper - publikacja naukowa 
+
+sabbatical (leave) - urlop naukowy 
+
+seminar - seminarium naukowe 
+
+Students Office - dziekanat  
+
+take a gap year - zrobić rok przerwy między szkołą a studiami 
+
+term paper - praca semestralna  
+
+tuition / school / college/ university fees - czesne  
+
+tutorial - konsultacje, seminarium  
+
+undergraduate - student, studentka pierwszych trzech lat (przed uzyskaniem licencjatu) 
+
+University of Technology - politechnika 
+
+18 
+
+Anthropology - antropologia 
+
+Business Studies - zarządzanie / przedsiębiorczość 
+
+Economics - ekonomia 
+
+Engineering - inżynieria 
+
+Fine Arts - sztuki piękne 
+
+Information Technology - informatyka 
+
+Law - prawo 
+
+Linguistics - językoznawstwo 
+
+Psychology - psychologia 
+
+Social Sciences - nauki społeczne 
+
+Sociology - socjologia
+"""
+o = []
+t = 6
+x = [z.strip() for z in x.split("\n") if z != ""]
+
+print("INSERT INTO `fiszki_fiszka`(`id`, `podgrupa_id`, `pl`, `de`) VALUES ")
+
+for idx, line in enumerate(x):
+    if(line.isdigit()):
+        t = int(line)
+    else:
+        
+        pl = line.split("-")[-1].strip()
+        de = ("-".join(line.split("-")[:-1])).strip()
+        
+        print(f"({t},\"{pl}\",\"{de}\"),")
+    
+        
+print(";")        
+        
+
+
+#print(x)
